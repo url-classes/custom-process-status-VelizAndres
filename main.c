da217174b9a71b966f8a9043ebb72ea815f16219
VelizAndres <dandresveliz18@gmail.com>
2022-02-13 22:58:46 -0600

    Tarea1

--- main.c
+++ main.c
@@ -1,23 +1,38 @@
+//Tarea Diego Andres Veliz Arauz
+
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include<time.h>
+
 
 int main()
 {
+    time_t t;  
+    time(&t);
+
     int fork_result;
+    
+    printf("Programa de resgistro de Procesos\n\n");
+
+    printf("%-35s %-8s %-8s %30s\n", "Process Type", "PID", "PPID", "Time");
+    printf("%-35s %-8d %-8d %30s", "System Proccess", getpid() , getppid(), ctime(&t));
+
     fork_result = fork();
-    if (fork_result >= 1)
-    {
-        printf("Soy el proceso padre\n");
+    if(fork_result==0)
+    { 
+     printf("%-35s %-8d %-8d %30s", "System Proccess", getpid() , getppid(), ctime(&t));
     }
-    else if (fork_result == 0)
-    {
-        printf("Soy el proceso hijo\n");
+    fork_result = fork();
+    if(fork_result==0)
+    { 
+     printf("%-35s %-8d %-8d %30s", "System Proccess", getpid() , getppid(), ctime(&t));
+
     }
-    else
-    {
-        printf("Error al crear el proceso\n");
-    }
-    return 0;
+    fork_result = fork();
+    if(fork_result==0)
+    { 
+     printf("%-35s %-8d %-8d %30s", "System Proccess", getpid() , getppid(), ctime(&t));
+
+    }        
 }
